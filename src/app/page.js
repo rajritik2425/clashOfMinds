3569d7015a971ae6c94e086794a0ca38abc790f1
PW <pw@PWL-CO-45037.local>
2025-05-29 03:47:16 +0530

    added profile

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,9 @@
 'use client'
 import HomeGameInterface from "./components/game-interface-home";
-import { useAuth } from "./utils/AuthContext";
 
 export default function Home() {
-  const {isAuthenticated} = useAuth();
-  console.log(token);
-  if(!isAuthenticated){
-    return <>
-    You are not authenticated
-    </>
-  }
   return (
     <>
-
       <HomeGameInterface />
     </>
   );
